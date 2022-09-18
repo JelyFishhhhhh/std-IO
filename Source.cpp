104db74f806a5da82af5edb800d8af3f7e61368c
JelyFishhhhhh_ _ <dlanfongtwb@gmail.com>
2022-09-18 22:32:41 +0800

    File Update

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#pragma warning( disable : 4996 )
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int a, b;
+	freopen("01.input", "r", stdin);
+	freopen("01.output", "w", stdout);
+	while (cin >> a >> b) {
+		fclose(stdin);
+		int sum = a + b;
+		cout << sum;
+		fclose(stdin);
+		fclose(stdout);
+	}
+}
